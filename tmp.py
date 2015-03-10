b543af9f4d2b031f1c2bfae4a3dad7370591ce96
Andrew Savchenko <art@artaman.net>
2015-03-10 21:48:32 +0800

    Delete tmp.py

--- tmp.py
+++
@@ -1,8 +0,0 @@
-if seq_size > 0:
-    if self.pathBool.value() is False:
-        constr = seq_niceName
-    else:
-        constr = sequence
-else:
-    seq_errors += 1
-print "* " + constr + " .... " + sconvert(seq_size)
